606c51d4be3d7e734cdcaa38921718ce60ce4fe3
Daniel Dye <daniel@seekster.co>
2019-12-12 03:34:48 +0700

    Increase button size on Snackbar

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -117,7 +117,6 @@ const TodoList = () => {
         action={
           <Button
             color='primary'
-            size='small'
             onClick={handleUndo}
           >
             UNDO
